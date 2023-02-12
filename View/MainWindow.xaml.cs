987249c5a5f1e84ca67a37d99906f364cd670b28
Tom <tom.a.antoine@free.fr>
2023-02-12 22:15:18 +0100

    Update Menu

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -34,5 +34,15 @@ namespace EasySaveV2
         {
 
         }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
