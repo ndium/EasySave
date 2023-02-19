bd6a3bf37772df2f50428d236ae6398edc4e1419
Paul <louagie27@gmail.com>
2023-02-19 16:11:54 +0100

    finish file encrypt

--- View/FileEncryptView.xaml.cs
+++ View/FileEncryptView.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using EasySave.Model;
 using EasySave.View_Model;
+using EasySaveV2.Model;
 using EasySaveV2.View_Model;
 
 namespace EasySaveV2.View
@@ -24,22 +25,45 @@ namespace EasySaveV2.View
     public partial class FileEncryptView : Page
     {
         public FileEncryptViewModel _fileEncryptViewModel { get; set; }
+        public List<Extension> list;
         public FileEncryptView()
         {
             InitializeComponent();
             DataContext = this;
-            List<Extension> list = _fileEncryptViewModel.GetList();
-            ExtendGrid.ItemsSource = list;
+            _fileEncryptViewModel = new FileEncryptViewModel();
+            refresh();
         }
 
+        //Button ajouter
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            string extensionName = InputExt.Text;
+            _fileEncryptViewModel.addExtension(extensionName);
+            InputExt.Text = "";
+            refresh();
         }
 
+        //Bouton supprimer
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            //Pour pouvoir supprimer en selectionnant
+            foreach (Extension item in ExtendGrid.SelectedItems)
+            {
+                _fileEncryptViewModel.removeExtension(item.Name);
+            }
 
+            //Supprimer grace au input
+            string extensionName = InputExt.Text;
+            _fileEncryptViewModel.removeExtension(extensionName);
+            InputExt.Text = "";
+            refresh();
+        }
+
+        //Refresh la page
+        private void refresh()
+        {
+            list = _fileEncryptViewModel.GetList();
+            ExtendGrid.ItemsSource = list;
         }
     }
 }
